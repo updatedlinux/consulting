8e85d7d58dd8071e06aa6c19748a8a546ac5d01b
Jonathan Melendez <soyjonnymelendez@gmail.com>
2025-09-23 18:17:05 -0400

    fix

--- condo360-surveys/swagger/swagger.js
+++ condo360-surveys/swagger/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/polls',
+        url: 'https://api.bonaventurecclub.com/polls',
         description: 'Development server',
       },
     ],
